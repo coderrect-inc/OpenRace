bc1521acc51e6275dfad94878bffa952df6efc63
funemy <liyzunique@gmail.com>
2021-03-17 18:36:46 -0500

    change doc-contributing to a better name

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,6 +2,6 @@ module.exports = {
   someSidebar: {
     'Quick Start': ['doc-get-started', 'doc-hello-world'],
     'User Guide': ['doc-workflow'],
-    Developers: ['doc-style-guide', 'doc-overview', 'doc-contributing'],
+    Developers: ['doc-style-guide', 'doc-overview', 'doc-adding-new-features'],
   },
 };
