5f46b008e3b2c7aa9abe213729a556b5691ebfb5
funemy <liyzunique@gmail.com>
2021-03-17 20:10:50 -0500

    add docs for contributing documentations

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
-  someSidebar: {
+  openraceSidebar: {
     'Quick Start': ['doc-get-started', 'doc-hello-world'],
     'User Guide': ['doc-workflow'],
-    'Developer Guide': ['doc-style-guide', 'doc-overview', 'doc-adding-new-features', 'doc-developing'],
+    'Developer Guide': ['doc-style-guide', 'doc-overview', 'doc-adding-new-features', 'doc-developing', 'doc-contribute-to-docs'],
   },
 };
