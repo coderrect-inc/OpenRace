38ad3fdbc65914ee4f568fe5fd5ebdd99e787805
Brad Swain <brad@coderrect.com>
2021-04-18 18:25:18 -0500

    add omp single test

--- tests/unit/Analysis/OmpArrayIndex.test.cpp
+++ tests/unit/Analysis/OmpArrayIndex.test.cpp
@@ -156,3 +156,86 @@ declare void @__kmpc_fork_call(%struct.ident_t*, i32, void (i32*, i32*, ...)*, .
   check_not_same_team(*threads.at(2), *threads.at(3));
   check_not_same_team(*threads.at(2), *threads.at(4));
 }
+
+TEST_CASE("OpenMP inSameSingleRegion Analysis") {
+  const char *ModuleString = R"(
+%struct.ident_t = type { i32, i32, i32, i32, i8* }
+
+@count = dso_local global i32 0, align 4
+@0 = private unnamed_addr constant [23 x i8] c";unknown;unknown;0;0;;\00", align 1
+@1 = private unnamed_addr constant %struct.ident_t { i32 0, i32 2, i32 0, i32 0, i8* getelementptr inbounds ([23 x i8], [23 x i8]* @0, i32 0, i32 0) }, align 8
+
+; Function Attrs: noinline nounwind optnone sspstrong uwtable
+define dso_local i32 @main() {
+entry:
+  call void (%struct.ident_t*, i32, void (i32*, i32*, ...)*, ...) @__kmpc_fork_call(%struct.ident_t* @1, i32 0, void (i32*, i32*, ...)* bitcast (void (i32*, i32*)* @.omp_outlined. to void (i32*, i32*, ...)*))
+  ret i32 0
+}
+
+; Function Attrs: noinline norecurse nounwind optnone sspstrong uwtable
+define internal void @.omp_outlined.(i32* noalias %.global_tid., i32* noalias %.bound_tid.) {
+entry:
+  %.kmpc_loc.addr = alloca %struct.ident_t
+
+  %start1 = call i32 @__kmpc_single(%struct.ident_t* %.kmpc_loc.addr, i32 0)
+  %0 = load i32, i32* @count, align 4
+  call void @__kmpc_end_single(%struct.ident_t* %.kmpc_loc.addr, i32 0)
+  
+  %inc = add nsw i32 %0, 1
+
+  %start2 = call i32 @__kmpc_single(%struct.ident_t* %.kmpc_loc.addr, i32 0)
+  store i32 %inc, i32* @count, align 4
+  call void @__kmpc_end_single(%struct.ident_t* %.kmpc_loc.addr, i32 0)
+
+  ret void
+}
+
+; Function Attrs: nounwind
+declare void @__kmpc_fork_call(%struct.ident_t*, i32, void (i32*, i32*, ...)*, ...)
+declare dso_local void @__kmpc_end_single(%struct.ident_t*, i32)
+declare dso_local i32 @__kmpc_single(%struct.ident_t*, i32)
+
+)";
+  llvm::LLVMContext Ctx;
+  llvm::SMDiagnostic Err;
+  auto module = llvm::parseAssemblyString(ModuleString, Err, Ctx);
+  if (!module) {
+    Err.print("error", llvm::errs());
+    FAIL("no module");
+  }
+
+  race::ProgramTrace program(module.get());
+  race::OmpArrayIndexAnalysis arrayIndexAnalysis;
+
+  auto const &threads = program.getThreads();
+  REQUIRE(threads.size() == 3);
+
+  auto check_same_team = [&arrayIndexAnalysis](const race::ThreadTrace &t1, const race::ThreadTrace &t2) {
+    for (auto const &e1 : t1.getEvents()) {
+      for (auto const &e2 : t2.getEvents()) {
+        CHECK(arrayIndexAnalysis.inSameTeam(e1.get(), e2.get()));
+      }
+    }
+  };
+
+  auto check_not_same_team = [&arrayIndexAnalysis](const race::ThreadTrace &t1, const race::ThreadTrace &t2) {
+    for (auto const &e1 : t1.getEvents()) {
+      for (auto const &e2 : t2.getEvents()) {
+        CHECK_FALSE(arrayIndexAnalysis.inSameTeam(e1.get(), e2.get()));
+      }
+    }
+  };
+  llvm::errs() << program << "\n";
+
+  auto const &e11 = program.getEvent(1, 1);
+  auto const &e14 = program.getEvent(1, 4);
+
+  auto const &e21 = program.getEvent(2, 1);
+  auto const &e24 = program.getEvent(2, 4);
+
+  CHECK(arrayIndexAnalysis.inSameSingleBlock(e11, e21));
+  CHECK(arrayIndexAnalysis.inSameSingleBlock(e14, e24));
+
+  CHECK_FALSE(arrayIndexAnalysis.inSameSingleBlock(e11, e24));
+  CHECK_FALSE(arrayIndexAnalysis.inSameSingleBlock(e14, e21));
+}
