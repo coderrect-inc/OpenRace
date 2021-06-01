51ff1d004ce52d2714ca0aed40d120a54ccac2cf
RohanKheraCR <85179050+RohanKheraCR@users.noreply.github.com>
2021-06-01 15:59:58 -0500

    Create single-message-printer.c

---
+++ tests/data/integration/openmp/single-message-printer.c
@@ -0,0 +1,23 @@
+ #include <stdio.h>
+
+ void work1() {}
+ void work2() {}
+
+ void single_example()
+ {
+ #pragma omp parallel
+ {
+ #pragma omp single
+ printf("Beginning work1.\n");
+
+ work1();
+
+ #pragma omp single
+ printf("Finishing work1.\n");
+
+ #pragma omp single
+ printf("Finished work1 and beginning work2.\n");
+
+ work2();
+ }
+ }
