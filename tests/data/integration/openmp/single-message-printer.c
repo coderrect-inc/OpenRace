c36c1abac5dd2a1371ce19394c185735e8a8d617
RohanKheraCR <you@example.com>
2021-06-04 19:11:01 +0000

    Restoring normalcy

--- tests/data/integration/openmp/single-message-printer.c
+++ tests/data/integration/openmp/single-message-printer.c
@@ -4,18 +4,22 @@
     int total = 0;
     #pragma omp parallel
     {
-    #pragma omp single
-    printf("Beginning work1.\n");
-    total+=2;
+        #pragma omp single
+        {
+            printf("Beginning work1.\n");
+            total+=2;
+        }
 
-    #pragma omp single
-    printf("Finishing work1.\n");
+        #pragma omp single
+        printf("Finishing work1.\n");
 
-    #pragma omp single
-    printf("Finished work1 and beginning work2.\n");
-    total -= 2;
-    }
- }
+        #pragma omp single 
+        {
+            printf("Finished work1 and beginning work2.\n");
+            total -= 2;
+        }
+    }       
+}
 
  int main () {
      single_example();
