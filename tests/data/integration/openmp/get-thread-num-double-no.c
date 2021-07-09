4ce6eae530eb3ef508c7ce262417855c2d36ca22
Brad Swain <brad@coderrect.com>
2021-07-09 16:38:16 -0500

    fix get_thread_num bug

---
+++ tests/data/integration/openmp/get-thread-num-double-no.c
@@ -0,0 +1,21 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main() {
+  int counter = 0;
+
+#pragma omp parallel
+  {
+    int tid = omp_get_thread_num();
+
+    if (tid == 1) {
+      counter++;
+    }
+
+    if (tid == 1) {
+      counter++;
+    }
+  }
+
+  printf("%d\n", counter);
+}
