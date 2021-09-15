56e459ec956c2a14be453a4a37db07c36d86ecd3
Brad Swain <brad@coderrect.com>
2021-09-15 11:30:05 -0500

    clear unjoined tasks after joining

--- src/Trace/Build/OpenMPRuntime.cpp
+++ src/Trace/Build/OpenMPRuntime.cpp
@@ -54,15 +54,13 @@ bool OpenMPRuntime::preVisit(const std::shared_ptr<const IR> &ir, ThreadBuildSta
 
   // if omp join IR, omp barrier, or taskwait,  check for tasks that need to be joined
   if (ir->type == IR::Type::OpenMPBarrier || ir->type == IR::Type::OpenMPTaskWait || ir->type == IR::Type::OpenMPJoin) {
-    std::vector<std::unique_ptr<const Event>> newEvents;
-    newEvents.reserve(unjoinedTasks.size());
-
     for (auto const &task : unjoinedTasks) {
       auto taskJoin = std::make_shared<OpenMPTaskJoin>(task.forkIR);
       std::shared_ptr<const JoinIR> join(taskJoin, llvm::cast<JoinIR>(taskJoin.get()));
       state.events.push_back(
           std::make_unique<const JoinEventImpl>(join, state.einfo, state.events.size(), task.forkEvent));
     }
+    unjoinedTasks.clear();
 
     return false;
   }
